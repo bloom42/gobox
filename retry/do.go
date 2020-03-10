674f13b69049eb66d93072cb90104f6184747b1b
z0mbie42 <sylvain@kerkour.fr>
2020-03-10 15:19:08 +0100

    retry

---
+++ retry/do.go
@@ -0,0 +1,4 @@
+// Package retry provides a simple retry library
+// see https://github.com/avast/retry-go
+// configuration trough functional options: https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis
+package retry
