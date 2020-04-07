5e2d553dcecfe259d7ffb35a5a2321a323b83820
z0mbie42 <sylvain@kerkour.fr>
2020-04-07 16:16:20 +0200

    crypto/kdf

--- crypto/kdf/kdf_test.go
+++ crypto/kdf/kdf_test.go
@@ -7,8 +7,19 @@ import (
 func TestDeriveFromKeyKeyLen(t *testing.T) {
 	var err error
 	key := []byte("some random data")
+
 	_, err = DeriveFromKey(key, 128)
 	if err == nil {
 		t.Error("Accept invalid keyLen")
+	}
+
+	_, err = DeriveFromKey(key, 65)
+	if err == nil {
+		t.Error("Accept invalid keyLen")
+	}
+
+	_, err = DeriveFromKey(key, 64)
+	if err != nil {
+		t.Error("Reject valid keyLen")
 	}
 }
