34cbf7fbf30c1e12e2eb05b7e2c202b7c41148d1
z0mbie42 <sylvain@kerkour.fr>
2020-04-07 22:18:29 +0200

    crypto/curve25519

---
+++ crypto/curve25519/curve25519.go
@@ -0,0 +1,22 @@
+package curve25519
+
+import (
+	"fmt"
+
+	"gitlab.com/bloom42/lily/crypto/rand"
+	"golang.org/x/crypto/curve25519"
+)
+
+func NewKeyPair() (publicKey, privateKey []byte, err error) {
+	privateKey = make([]byte, curve25519.ScalarSize)
+
+	if _, err = rand.Reader().Read(privateKey); err != nil {
+		return nil, nil, fmt.Errorf("internal error: %v", err)
+	}
+	publicKey, err = curve25519.X25519(privateKey, curve25519.Basepoint)
+	if err != nil {
+		return nil, nil, fmt.Errorf("internal error: %v", err)
+	}
+
+	return
+}
