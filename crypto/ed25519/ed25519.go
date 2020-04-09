8a3680990a11a9fea790b601c5a685f1a29ed408
z0mbie42 <sylvain@kerkour.fr>
2020-04-09 10:23:05 +0200

    crypto/ed25519: PublicKey.ToCurve25519PublicKey

--- crypto/ed25519/ed25519.go
+++ crypto/ed25519/ed25519.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha512"
 	"errors"
 	"io"
+	"math/big"
 
 	"golang.org/x/crypto/curve25519"
 )
@@ -32,6 +33,41 @@ func (public PublicKey) Verify(message, signature []byte) (bool, error) {
 	}
 
 	return ed25519.Verify(ed25519.PublicKey(public), message, signature), nil
+}
+
+var curve25519P, _ = new(big.Int).SetString("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10)
+
+// ToCurve25519PublicKey returns the corresponding Curve25519 public key
+// see here for more details: https://blog.filippo.io/using-ed25519-keys-for-encryption/
+func (public PublicKey) ToCurve25519PublicKey() []byte {
+	// taken from https://github.com/FiloSottile/age/blob/f0f8092d60bb96737fa096c29ec6d8adb5810390/internal/age/ssh.go#L174
+
+	// ed25519.PublicKey is a little endian representation of the y-coordinate,
+	// with the most significant bit set based on the sign of the x-coordinate.
+	bigEndianY := make([]byte, PublicKeySize)
+	for i, b := range public {
+		bigEndianY[PublicKeySize-i-1] = b
+	}
+	bigEndianY[0] &= 0b0111_1111
+
+	// The Montgomery u-coordinate is derived through the bilinear map
+	//
+	//     u = (1 + y) / (1 - y)
+	//
+	// See https://blog.filippo.io/using-ed25519-keys-for-encryption.
+	y := new(big.Int).SetBytes(bigEndianY)
+	denom := big.NewInt(1)
+	denom.ModInverse(denom.Sub(denom, y), curve25519P) // 1 / (1 - y)
+	u := y.Mul(y.Add(y, big.NewInt(1)), denom)
+	u.Mod(u, curve25519P)
+
+	out := make([]byte, curve25519.PointSize)
+	uBytes := u.Bytes()
+	for i, b := range uBytes {
+		out[len(uBytes)-i-1] = b
+	}
+
+	return out
 }
 
 // PrivateKey is the type of Ed25519 private keys. It implements crypto.Signer.
