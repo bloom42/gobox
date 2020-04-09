0e60824b144d4a71a773a110f6acfbbe04f75c74
z0mbie42 <sylvain@kerkour.fr>
2020-04-09 15:57:48 +0200

    crypto: add tests for hashs

--- crypto/hash.go
+++ crypto/hash.go
@@ -18,9 +18,27 @@ const (
 	HashSize512 HashSize = 64
 )
 
-// NewHash returns a new hash.Hash computing the BLAKE2b checksum with a custom length.
+// NewHash returns a new `hash.Hash` computing the BLAKE2b checksum with a custom length.
 // size can be a value between 1 and 64.
 // It is highly recommended to use values equal or greater than 32.
 func NewHash(size HashSize) (hash.Hash, error) {
 	return blake2b.New(int(size), nil)
+}
+
+// Hash256 returns the BLAKE2b-256 checksum of the data.
+func Hash256(data []byte) []byte {
+	sum := blake2b.Sum256(data)
+	return sum[:]
+}
+
+// Hash384 returns the BLAKE2b-384 checksum of the data.
+func Hash384(data []byte) []byte {
+	sum := blake2b.Sum384(data)
+	return sum[:]
+}
+
+// Hash512 returns the BLAKE2b-512 checksum of the data.
+func Hash512(data []byte) []byte {
+	sum := blake2b.Sum512(data)
+	return sum[:]
 }
