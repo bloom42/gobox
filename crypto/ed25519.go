e408570491029b2cb79e9f9fe8954f3096fd8edb
Sylvain Kerkour <sylvain@kerkour.fr>
2020-06-25 14:16:27 +0200

    crypto/ed25519: add ToCurve25519 functions

--- crypto/ed25519.go
+++ crypto/ed25519.go
@@ -3,8 +3,10 @@ package crypto
 import (
 	"crypto"
 	"crypto/ed25519"
+	"crypto/sha512"
 	"errors"
 	"io"
+	"math/big"
 )
 
 const (
@@ -37,41 +39,41 @@ func (publicKey Ed25519PublicKey) Verify(message, signature []byte) (bool, error
 	return ed25519.Verify(ed25519.PublicKey(publicKey), message, signature), nil
 }
 
-// var curve25519P, _ = new(big.Int).SetString("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10)
+var curve25519P, _ = new(big.Int).SetString("57896044618658097711785492504343953926634992332820282019728792003956564819949", 10)
 
-// // ToCurve25519PublicKey returns the corresponding Curve25519 public key.
-// //
-// // See here for more details: https://blog.filippo.io/using-ed25519-keys-for-encryption
-// func (publicKey PublicKey) ToCurve25519PublicKey() []byte {
-// 	// taken from https://github.com/FiloSottile/age/blob/master/internal/agessh/agessh.go#L179
+// ToCurve25519PublicKey returns the corresponding Curve25519 public key.
+//
+// See here for more details: https://blog.filippo.io/using-ed25519-keys-for-encryption
+func (publicKey Ed25519PublicKey) ToCurve25519PublicKey() Curve25519PublicKey {
+	// taken from https://github.com/FiloSottile/age/blob/master/internal/agessh/agessh.go#L179
 
-// 	// ed25519.PublicKey is a little endian representation of the y-coordinate,
-// 	// with the most significant bit set based on the sign of the x-coordinate.
-// 	bigEndianY := make([]byte, PublicKeySize)
-// 	for i, b := range publicKey {
-// 		bigEndianY[PublicKeySize-i-1] = b
-// 	}
-// 	bigEndianY[0] &= 0b0111_1111
+	// ed25519.PublicKey is a little endian representation of the y-coordinate,
+	// with the most significant bit set based on the sign of the x-coordinate.
+	bigEndianY := make([]byte, Ed25519PublicKeySize)
+	for i, b := range publicKey {
+		bigEndianY[Ed25519PublicKeySize-i-1] = b
+	}
+	bigEndianY[0] &= 0b0111_1111
 
-// 	// The Montgomery u-coordinate is derived through the bilinear map
-// 	//
-// 	//     u = (1 + y) / (1 - y)
-// 	//
-// 	// See https://blog.filippo.io/using-ed25519-keys-for-encryption.
-// 	y := new(big.Int).SetBytes(bigEndianY)
-// 	denom := big.NewInt(1)
-// 	denom.ModInverse(denom.Sub(denom, y), curve25519P) // 1 / (1 - y)
-// 	u := y.Mul(y.Add(y, big.NewInt(1)), denom)
-// 	u.Mod(u, curve25519P)
+	// The Montgomery u-coordinate is derived through the bilinear map
+	//
+	//     u = (1 + y) / (1 - y)
+	//
+	// See https://blog.filippo.io/using-ed25519-keys-for-encryption.
+	y := new(big.Int).SetBytes(bigEndianY)
+	denom := big.NewInt(1)
+	denom.ModInverse(denom.Sub(denom, y), curve25519P) // 1 / (1 - y)
+	u := y.Mul(y.Add(y, big.NewInt(1)), denom)
+	u.Mod(u, curve25519P)
 
-// 	out := make([]byte, curve25519.PointSize)
-// 	uBytes := u.Bytes()
-// 	for i, b := range uBytes {
-// 		out[len(uBytes)-i-1] = b
-// 	}
+	out := make([]byte, Curve25519PublicKeySize)
+	uBytes := u.Bytes()
+	for i, b := range uBytes {
+		out[len(uBytes)-i-1] = b
+	}
 
-// 	return out
-// }
+	return out
+}
 
 // Ed25519PrivateKey is the type of Ed25519 private keys. It implements crypto.Signer.
 type Ed25519PrivateKey ed25519.PrivateKey
@@ -89,16 +91,16 @@ func (privateKey Ed25519PrivateKey) Sign(rand io.Reader, message []byte, opts cr
 	return ed25519.Sign(ed25519.PrivateKey(privateKey), message), nil
 }
 
-// // ToCurve25519PrivateKey returns a corresponding Curve25519 private key.
-// //
-// // See here for more details: https://blog.filippo.io/using-ed25519-keys-for-encryption
-// func (privateKey PrivateKey) ToCurve25519PrivateKey() []byte {
-// 	// taken from https://github.com/FiloSottile/age/blob/292c3aaeea0695dbba356dfe18a70f10efb17d75/internal/agessh/agessh.go#L294
-// 	h := sha512.New()
-// 	h.Write(ed25519.PrivateKey(privateKey).Seed())
-// 	out := h.Sum(nil)
-// 	return out[:curve25519.ScalarSize]
-// }
+// ToCurve25519PrivateKey returns a corresponding Curve25519 private key.
+//
+// See here for more details: https://blog.filippo.io/using-ed25519-keys-for-encryption
+func (privateKey Ed25519PrivateKey) ToCurve25519PrivateKey() Curve25519PrivateKey {
+	// taken from https://github.com/FiloSottile/age/blob/292c3aaeea0695dbba356dfe18a70f10efb17d75/internal/agessh/agessh.go#L294
+	h := sha512.New()
+	h.Write(privateKey.Seed())
+	out := h.Sum(nil)
+	return out[:Curve25519PrivateKeySize]
+}
 
 // Public returns the Ed25519PublicKey corresponding to priv.
 func (privateKey Ed25519PrivateKey) Public() Ed25519PublicKey {
--- crypto/ed25519_test.go
+++ crypto/ed25519_test.go
@@ -21,3 +21,23 @@ func TestGenerateEd25519Keypair(t *testing.T) {
 		t.Error("Generated public key is empty")
 	}
 }
+
+func TestEd25519KeypairToCurve25519(t *testing.T) {
+	ed25519PublicKey, ed25519PrivateKey, err := GenerateEd25519KeyPair()
+	if err != nil {
+		t.Error(err)
+	}
+
+	curve25519PrivateKey := ed25519PrivateKey.ToCurve25519PrivateKey()
+
+	curve25519PublicKey, err := curve25519PrivateKey.Public()
+	if err != nil {
+		t.Error(err)
+	}
+
+	curve25519PublicKey2 := ed25519PublicKey.ToCurve25519PublicKey()
+
+	if !ConstantTimeCompare(curve25519PublicKey, curve25519PublicKey2) {
+		t.Errorf("curve25519PublicKey (%x) != curve25519PublicKey2 (%x)", curve25519PublicKey, curve25519PublicKey2)
+	}
+}
